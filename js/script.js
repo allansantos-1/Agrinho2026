7beea70683ab1afe8133adfdfb0dbf97dbf4d694
allansantos-1 <allan.santos14@escola.pr.gov.br>
2026-05-29 19:38:37 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Todo o JavaScript ficará aqui
+
+const btnPesquisar = document.getElementById("btnPesquisar");
+
+btnPesquisar.addEventListener("click", () => {
+    alert("Função de pesquisa será implementada.");
+});
